a4154f7612dabafa5effe0587a7071f4011256a1
Marco Starker <marco.starker@gmx.net>
2024-07-05 10:22:10 +0200

    logging

--- src/index.js
+++ src/index.js
@@ -2,37 +2,23 @@ import https from 'https';
 
 const HEADERS = ['Content-Type', 'X-GitHub-Delivery', 'X-GitHub-Event', 'X-GitHub-Hook-ID', 'X-GitHub-Hook-Installation-Target-ID', 'X-GitHub-Hook-Installation-Target-Type', 'X-Hub-Signature', 'X-Hub-Signature-256'];
 const MAPPING = Object.entries({
-    drone() {
-        return {
+    drone(event) {
+        return [{
+            ...this.options(event),
             hostname: process.env.GDC_DRONE_HOST ?? 'drone.dasred.de',
             path:     process.env.GDC_DRONE_PATH ?? '/hook',
-        };
+        }];
     },
     portainer(event, path) {
-        return {
+        return [{
+            ...this.options(event, path),
             hostname: process.env.GPC_PORTAINER_HOST ?? 'portainer.dasred.de',
             path:     (process.env.GPC_PORTAINER_PATH ?? '/api/stacks/webhooks/') + path.split('/').pop(),
-        };
-    }
-});
+        }];
+    },
 
-function LOG(...args) {
-    console.log(...args);
-}
-
-export const handler = (event) => {
-    return new Promise((resolve, reject) => {
-        LOG('EVENT', event);
-
-        const helper = MAPPING.find(([path]) => event.path.startsWith('/' + path));
-        if (helper === undefined) {
-            LOG('ERROR', {statusCode: 405, message: 'Mapping not found'});
-            resolve({statusCode: 405, message: 'Mapping not found'});
-            return;
-        }
-
-        const options = {
-            ...helper[1](event, event.path.substring(helper[0])),
+    options(event) {
+        return {
             port:               443,
             method:             event.httpMethod,
             rejectUnauthorized: false,
@@ -46,36 +32,61 @@ export const handler = (event) => {
                 },
                 {'accept': '*/*'}
             ),
-        };
+        }
+    }
+});
 
-        LOG('REQUEST', options);
+function LOG(...args) {
+    console.log(...args);
+}
 
-        const request = https.request(options,
-            /** @param {IncomingMessage} response */
-            (response) => {
-                let body = '';
-                response.setEncoding('utf8');
-                response.on('data', (chunk) => body += chunk);
-                response.on('end', () => {
-                    LOG('RESPONSE', {
-                        statusCode:    response.statusCode || 500,
-                        statusMessage: response.statusMessage,
-                        body:          body,
-                        headers:       response.rawHeaders
-                    });
+export const handler = async (event) => {
+    LOG('EVENT', event);
 
-                    if (response.status >= 400) {
-                        return reject({
-                            statusCode: response.statusCode || 500,
-                            body:       body,
+    const helper = MAPPING.find(([path]) => event.path.startsWith('/' + path));
+    if (helper === undefined) {
+        LOG('ERROR', {statusCode: 405, message: 'Mapping not found'});
+        return {statusCode: 405, message: 'Mapping not found'};
+    }
+
+    const results = await Promise.all(helper[1](event, event.path.substring(helper[0])).map((options, index) => {
+        return new Promise((resolve) => {
+            LOG(`REQUEST #${index}`, options);
+
+            const request = https.request(options,
+                /** @param {IncomingMessage} response */
+                (response) => {
+                    let body = '';
+                    response.setEncoding('utf8');
+                    response.on('data', (chunk) => body += chunk);
+                    response.on('end', () => {
+                        LOG(`RESPONSE #${index}`, {
+                            statusCode:    response.statusCode || 500,
+                            statusMessage: response.statusMessage,
+                            body:          body,
+                            headers:       response.rawHeaders
                         });
-                    }
 
-                    resolve({statusCode: 200});
-                });
-            }
-        );
-        request.write(event.body);
-        request.end();
-    });
+                        if (response.status >= 400) {
+                            return resolve({
+                                statusCode: response.statusCode || 500,
+                                body:       body,
+                            });
+                        }
+
+                        resolve({statusCode: 204});
+                    });
+                }
+            );
+            request.write(event.body);
+            request.end();
+        });
+    }));
+
+    const resultFailed = results.find(({statusCode}) => statusCode !== 204);
+    if (resultFailed) {
+        return resultFailed;
+    }
+
+    return {statusCode: 204};
 };
